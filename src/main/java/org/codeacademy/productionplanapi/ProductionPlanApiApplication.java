50a8f4b0efc565b6bdaf8f79d7e3958f7204b583
Dovydas <ddorofejevas@yahoo.com>
2025-04-17 23:56:50 +0300

    postman collection

--- src/main/java/org/codeacademy/productionplanapi/ProductionPlanApiApplication.java
+++ src/main/java/org/codeacademy/productionplanapi/ProductionPlanApiApplication.java
@@ -1,6 +1,5 @@
 package org.codeacademy.productionplanapi;
 
-import org.codeacademy.productionplanapi.security.JwtService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
